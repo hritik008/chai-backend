807be668da624e1a5403537bad43ed9209a0316d
Hritik Ranjan <hritik.ranjan@geminisolutions.com>
2026-01-07 00:59:57 +0530

    utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (fn) => async (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+
+//     } catch (error) {
+//         res.status(error.status || 500).json({
+//             success: false,
+//             message: error.message,
+//         });
+//     }
+// }
