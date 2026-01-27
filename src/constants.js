16323c993a1aca0977fbf65219005e0cce1c2de8
Hritik Ranjan <hritik.ranjan@geminisolutions.com>
2026-01-28 01:09:42 +0530

    fix all register controller bugs

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "chaibackend";
+export const DB_NAME = "videotube";
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,10 +15,13 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     //file has been upload successfully
-    console.log("file uploaded on cloudinary", response.url);
+    // console.log("file uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
+
+    //console.log("Cloudinary response:", response);
     return response;
   } catch (error) {
-    fs.unlink(localFilePath); //remove the locally saved temporary file as the upload operation got failed
+    fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation got failed
     return null;
   }
 };
